1d9c4004a933c1e024f0dda95d95d5c24730b5ae
Adrianna Chang <adrianna.chang@shopify.com>
2021-02-10 11:46:54 -0500

    Test sending a response to the client for our ec2 instance

--- challenge_2/http.rb
+++ challenge_2/http.rb
@@ -30,12 +30,34 @@ loop do
   puts "Request target: #{target}"
   puts "HTTP Version: #{http_version}"
 
-  # client.close
+  puts "Building response for client!"
+  # The status line is a single line of text containing:
+  # - The HTTP protocol version
+  # - The status code
+  # - Optionally, a piece of text to describe the status code
+  #   Question: Can you make this whatever you want, or will the client expect
+  #   the status code to match a given piece of text? When would you ever send
+  #   a "non-typical" piece of text here?
+  status_line = "#{http_version} #{status_codes[:ok]} #{status_codes_text[:ok]}\n"
 
-  # Read the rest of the HTTP message
-  # The rest is all headers
-  # We'll ignore this for now since we just want the request line
-  while !client.eof?
-    puts client.readline
-  end
+  # Feedback from Tom on whether my understanding here is correct:
+  # If you have multiple headers, they would each be on separate lines (ie. with a new
+  # line at the end of each header field)
+  # Then, we need ANOTHER new line to separate the set of header fields from the message body (see L55)
+  header_field = "Content-Type: text/plain\n"
+
+  # Build the message body
+  # From Tom in the doc: "The message body of a response message will be displayed by the browser"
+  # This is why it's important we specify the content type, so the browser knows
+  # how to display the information we send in the body
+  message_body = "Request method: #{method}, Request target: #{target}, HTTP Version: #{http_version}"
+
+  # Send response to client
+  client.write(status_line)
+  client.write(header_field)
+  # New line to separate the headers from the message body
+  client.write("\n")
+  client.write(message_body)
+
+  client.close
 end
