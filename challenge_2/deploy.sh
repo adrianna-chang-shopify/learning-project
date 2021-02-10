47bca3fd3c36a18662dc357afded694a333882fa
Adrianna Chang <adrianna.chang@shopify.com>
2021-02-10 11:56:24 -0500

    Moar deploy stuff

--- challenge_2/deploy.sh
+++ challenge_2/deploy.sh
@@ -1,2 +1,7 @@
 echo 'Deploying to production!'
+echo '🚀 Pushing changes'
 git push production main
+echo '♻️ Restarting service'
+ssh ubuntu@ec2-174-129-49-169.compute-1.amazonaws.com systemctl --user restart http
+echo '🔍 Fetching status'
+ssh ubuntu@ec2-174-129-49-169.compute-1.amazonaws.com systemctl --user status http
